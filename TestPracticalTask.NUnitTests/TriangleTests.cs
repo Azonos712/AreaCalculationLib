30f5fe02219f15dd4ad05dc0babc95cb61f47cbb
Azonos712 <nekoooolay@mail.ru>
2022-04-04 14:22:29 +0300

    Create tests for triangle

---
+++ TestPracticalTask.NUnitTests/TriangleTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestPracticalTask.Library;
+
+namespace TestPracticalTask.NUnitTests
+{
+    public class TriangleTests
+    {
+        [Test]
+        public void GoodAreaCalculation()
+        {
+            var _goodTriangle = new Triangle(3, 4, 5);
+            Assert.AreEqual(_goodTriangle.CalculateArea(), 6);
+        }
+
+        [Test]
+        public void BadAreaCalculation()
+        {
+            Assert.Throws<InvalidDataException>(() =>
+            {
+                var _badTriangle = new Triangle(1, 1, 5);
+            });
+        }
+
+    }
+}
